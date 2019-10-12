629ffb8a5d9212765973ba3e8480ac95f9d720bd
Xavier Chotard <xavier@helium>
2019-10-12 15:18:22 +0200

    data upgrade detection not necessary

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -16,20 +16,8 @@ if [ ! -e "/var/www/html/data/configuration/parametres.xml" ]; then
     cp -R /var/www/html/data.defaults/* /var/www/html/data
     chown -R www-data: /var/www/html/data
 else
-    echo "Data found - Checking version"
-        plx__version="$(grep 'PLX_VERSION' /var/www/html/core/lib/config.php | cut -d "'" -f 4)"
-        plx__installed="$(grep '"version"' /var/www/html/data/configuration/parametres.xml  | cut -f 2 -d ">" | cut -f 1 -d "<" )"
-        if [ "$plx__version" -gt "$plx__installed" ]; then
-            echo "Data upgrade is available"
-            echo "install.php will not be removed"
-        elif [ "$plx__version" -lt "$plx__installed" ]; then
-            echo "Woops, this blog has been created with a newer version of PluXml"
-            echo "Expect trouble..."
-        else
-            echo "Already up-to-date"
-            echo "install.php will be removed"
-            rm /var/www/html/install.php
-        fi
+    echo "Existing Data found ."
+    echo "install.php will be removed."
 fi
 
 # Set date.timezone if PHP_TIMEZONE is defined
